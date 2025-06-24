8e5981ccd808ad7e804a492fe7f203e16a03c1aa
Wolfearl <83833066+Wolfearl@users.noreply.github.com.>
2025-06-24 07:57:19 +0300

    Add task

---
+++ src/Problems/Java_Int_to_String.java
@@ -0,0 +1,43 @@
+package Problems;
+
+import java.security.Permission;
+import java.util.Scanner;
+
+public class Java_Int_to_String {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = Integer.toString(n);
+        try {
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
