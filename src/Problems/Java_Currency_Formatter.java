96815d414f2fe4d341e1c0aee873d1c3b37ffb23
Wolfearl <83833066+Wolfearl@users.noreply.github.com.>
2025-06-26 09:53:48 +0300

    Add task

---
+++ src/Problems/Java_Currency_Formatter.java
@@ -0,0 +1,30 @@
+package Problems;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class Java_Currency_Formatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = usFormat.format(payment);
+
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        String india = "Rs." + indiaFormat.format(payment).replace("$", "");
+
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = chinaFormat.format(payment);
+
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = franceFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
